465661638b6c4261d9cad7aa9ed94d2822ac6ff8
Mike <slavik433@gmail.com>
2022-06-02 08:49:19 +0300

    Updates

---
+++ precache-manifest.118af0e956b6fb32077cd833904ab688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabd0c5e79ff00a08d43fbeeea75107f",
+    "url": "/todolistAgain/index.html"
+  },
+  {
+    "revision": "055db66fa4b3cb730f03",
+    "url": "/todolistAgain/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todolistAgain/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolistAgain/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055db66fa4b3cb730f03",
+    "url": "/todolistAgain/static/js/main.054be8fd.chunk.js"
+  },
+  {
+    "revision": "79c7fb1e808b668078ed",
+    "url": "/todolistAgain/static/js/runtime-main.b852286f.js"
+  }
+]);
